7bdb7924074475b6dcc144337cb1bcf8c8c2f462
Mountok <themountok@gmail.com>
2024-05-31 14:35:20 +0300

    +

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Создаем роутер Gin
 	router := gin.Default()
+	os.Setenv("PORT_TEST_GO", "8080")
+	os.Setenv("TEST_NAME", "Islam")
+	port := os.Getenv("PORT_TEST_GO")
+	name := os.Getenv("TEST_NAME")
 
 	// Регистрируем обработчик для корневого пути
 	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.String(200, fmt.Sprintf("Hello, World, %s", name))
 	})
 
 	// Запускаем сервер на адресе 0.0.0.0:8080
-	router.Run("0.0.0.0:8080")
+	router.Run(fmt.Sprintf("0.0.0.0:%s", port))
 }
